0771cc281da4ed8f24e7ea103bd2e76c50f60fe9
Tom Windell <thomas@thinkst.com>
2026-07-03 15:27:56 +0200

    Simplify code

--- canarytools/models/canarytokens.py
+++ canarytools/models/canarytokens.py
@@ -6,7 +6,7 @@ from ..exceptions import InvalidParameterError
 import logging
 logger = logging.getLogger('canarytools')
 
-CANARYTOKENS_FETCH_PAGE_SIZE_LIMIT = 5000
+CANARYTOKENS_PAGE_SIZE_LIMIT = 2500
 
 class CanaryTokens(object):
     def __init__(self, console):
@@ -127,7 +127,7 @@ class CanaryTokens(object):
         return self.console.get('canarytoken/fetch', params, self.parse)
 
     def all(self, include_endpoints=True):
-        """Fetch all Canarytokens using the paginate endpoint
+        """Fetch all Canarytokens in batches using the paginate endpoint
 
         :return: A list of Canarytoken objects
         :rtype: List of :class:`CanaryToken <CanaryToken>` objects
@@ -144,27 +144,21 @@ class CanaryTokens(object):
 
         while True:
             params = {
-                'limit': CANARYTOKENS_FETCH_PAGE_SIZE_LIMIT,
+                'limit': CANARYTOKENS_PAGE_SIZE_LIMIT,
                 'render_transients': str(include_endpoints),
             }
+
             if cursor:
                 params['cursor'] = cursor
 
-            page_data = self.console.get('canarytokens/paginate', params, parser=lambda data: data)
-            page_tokens = page_data.get('canarytokens', [])
+            response = self.console.get('canarytokens/paginate', params, parser=dict)
 
-            for token in page_tokens:
-                all_tokens.append(CanaryToken.parse(self.console, token))
+            all_tokens.extend(
+                CanaryToken.parse(self.console, token)
+                for token in response.get('canarytokens', [])
+            )
 
-            if len(page_tokens) < CANARYTOKENS_FETCH_PAGE_SIZE_LIMIT:
-                break
-
-            cursor_data = page_data.get('cursor')
-            if isinstance(cursor_data, dict):
-                cursor = cursor_data.get('next')
-            else:
-                cursor = cursor_data
-
+            cursor = (response.get('cursor') or {}).get('next')
             if not cursor:
                 break
 
